b8ee98f72ed2eaa48206c8a8b47fbe4b0ac5d0fd
mark <markt2026@student.cis.edu.hk>
2024-03-06 09:00:37 +0800

    Quiz 2.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,29 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
 
         // call the first add method and store the result into a variable called resultOne.
+            int numOne = 10;
+                    int numTwo = 3;
+            int resultOne  = add( numOne, numTwo);
 
-        // print resultOne.
 
+            // print resultOne.
+            System.out.println(resultOne);
+        }
         // call the greeting method and store the result into a variable called resultTwo.
+        public static String greeting(String name)   {
+
+            return "Bonjour, " + name;
+        }
 
         // print resultTwo.
-
+        //System.out.println(resultTwo);
         // call the second add method and store the result into a variable called resultThree.
-
+    public static int resultThree(int numOne, int numTwo, int numThree, int numFour){
+        return numOne + numTwo + numThree + numFour;
         // print resultThree.
-
+        //System.out.println(resultThree);
         // call the printCapitalised method.
 
     }
@@ -24,6 +34,10 @@ public class Main {
      * @param numTwo - an integer
      * @return the sum of numOne and numTwo (an integer)
      */
+
+    public static int add(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
 
 
     /**
